890d4356b85398f333093db7d1a6e5fc10d5932d
wjalexan <67093567+wjalexan@users.noreply.github.com>
2022-06-14 12:42:05 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.configure(bg="gray")
+root.title("Hotel Check-in")
+
+
+label = Label(root, text="test", bg="gray", fg="white")
+label.grid(row=0,column=0)
+
+root.mainloop()
